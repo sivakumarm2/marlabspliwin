7e06f6aa946da700453a7f7c83b74724c559ffa3
sivakumarm2 <sivagithub@gmail.com>
2016-04-29 00:08:20 +0530

    Added Test project

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PliWindowsApp;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // input valuaes
+            int number1 = 10;
+            int number2 = 20;
+            int expected = 30;
+
+            int actual=PliWindowsApp.Calculation.AddNumbers(number1, number2);
+            Assert.AreEqual(expected, actual, "Result matching as expected");
+
+        }
+    }
+}
