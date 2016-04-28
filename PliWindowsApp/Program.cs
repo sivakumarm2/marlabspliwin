2c948b4ae9c93cd8952ff1cbbb276ca8c849f268
sivakumarm2 <sivagithub@gmail.com>
2016-04-29 00:08:45 +0530

    added test

--- PliWindowsApp/Program.cs
+++ PliWindowsApp/Program.cs
@@ -4,15 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace PliWindowsApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Continuous Integration using TeamCity");
-            Console.WriteLine("Please enter any key to exit!")
-            Console.Read();
+            Console.WriteLine("Please enter  number 1");
+            int i=int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number 2");
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of two numbers is");
+            Console.WriteLine(Calculation.AddNumbers(i, j));
+            Console.ReadLine();
+            
+        }
+
+        
+    }
+
+    public class Calculation
+    {
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
         }
     }
 }
